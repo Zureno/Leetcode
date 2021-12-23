ff82825f82ae5f32d1410183c1cab18d972e3f17
Zureno <pranshu21freak@gmail.com>
2021-12-23 17:12:53 -0500

    Solutions

--- 1200-MinimumAbsoluteDistance/solution.py
+++ 1200-MinimumAbsoluteDistance/solution.py
@@ -1,4 +1,4 @@
 def minimumAbsDifference(arr):
-	arr.sort()
-	rm = min(b-a for a,b in zip(arr,arr[1:]))
-	return ([a,b] for a,b in zip(arr,arr[1:)] if b-a == rm])
+	arr.sort()  #sort the array 
+	rm = min(b-a for a,b in zip(arr,arr[1:])) #use zip to find the minimum between two consecutive pairs.
+	return ([a,b] for a,b in zip(arr,arr[1:)] if b-a == rm]) #return the pair whose difference is equal to minimum.
